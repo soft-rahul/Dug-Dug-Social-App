68ce9a1301c060e5b845c52c9c2171bfcfe01a76
Rahul keshri <rkeshri2021@gmail.com>
2024-03-12 01:13:00 +0530

    Initial Commit

---
+++ src/lib/components/ui/Layout/Header.svelte
@@ -0,0 +1,11 @@
+<header>
+	<div class="max-w-screen-xl mx-auto py-2 border-b-2  border-dashed border-gray-200">
+		<section>
+			<div class="flex items-center gap-1">
+				<img src="/logo.png" alt="" class="h-5 w-5" />
+				<h1 class="font-bold">Duggup</h1>
+			</div>
+		</section>
+		<section></section>
+	</div>
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Header from '../lib/components/ui/Layout/Header.svelte';
+	import '../app.pcss';
+</script>
+
+<Header/>
+<slot />
