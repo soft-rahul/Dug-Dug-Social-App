52e865034bb917e84292ea386dbda773a6ae3d26
Rahul keshri <rkeshri2021@gmail.com>
2024-03-12 20:34:08 +0530

    Final Touch - Intro

--- src/lib/components/ui/Layout/Intro.svelte
+++ src/lib/components/ui/Layout/Intro.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
-    import {SquareArrowOutUpRight, Ellipsis} from 'lucide-svelte';
+	import { SquareArrowOutUpRight, Ellipsis } from 'lucide-svelte';
 	import * as Avatar from '$lib/components/ui/avatar';
+
+	let value = 'Follow';
+	let isActive = false;
+	const clickHandler = () => {
+		isActive = true;
+		setTimeout(() => {
+			value = value === 'Followed' ? 'Follow' : 'Followed';
+			isActive = false;
+		}, 600);
+	};
 </script>
 
 <main class="mx-auto my-6 max-w-screen-lg px-10 py-2">
 	<div class="my-4 flex items-center gap-4">
 		<section class="mr-4 flex flex-col items-center gap-2">
 			<div>
-				<Avatar.Root class="h-36 w-36 rounded-full border-2 border-gray-100">
+				<Avatar.Root class="h-36 w-36 rounded-full border-4 border-gray-200">
 					<Avatar.Image src="/picture.jpeg" alt="Krishna Kiran" />
 					<Avatar.Fallback>Krishan Kiran</Avatar.Fallback>
 				</Avatar.Root>
@@ -19,7 +29,7 @@
 		</section>
 
 		<section>
-			<section class="flex">
+			<section class="mb-5 flex">
 				<div class="p-4">
 					Co-Founder and CEO at Duggup - Social network for people in tech. Ex-Amazon Head of
 					Engineering. I write hot-takes on building a business, shipping delightful products and
@@ -36,18 +46,30 @@
 			<section class="flex justify-between pl-4">
 				<div>
 					<div
-						class="flex cursor-pointer items-center justify-between gap-2 rounded-md border-2 border-gray-600 px-4 py-2 hover:border-gray-200"
+						on:click={clickHandler}
+						class={`btn flex cursor-pointer items-center justify-between gap-2 rounded-md px-4 py-2 ${isActive ? 'color bg-blue-600 text-white shadow-none' : ''}`}
 					>
-						Follow
+						{value}
 					</div>
 				</div>
 				<div class="flex items-center">
-                    <span>My Website</span>
-                    <SquareArrowOutUpRight class="w-5 h-5"/>
-                    <Ellipsis class="h-7 w-7 ml-4" color="#bbb"/>
-                </div>
+					<span class="mr-1 cursor-pointer">My Website</span>
+					<SquareArrowOutUpRight class="h-5 w-5 cursor-pointer" />
+					<Ellipsis class="ml-4 h-7 w-7 cursor-pointer" color="#bbb" />
+				</div>
 			</section>
 		</section>
-
 	</div>
 </main>
+
+<style>
+	.btn {
+		border: 1px solid #000;
+		box-shadow: 0px 4px #000;
+		transition: all 200ms linear;
+	}
+	.btn:hover,
+	.btn:active {
+		box-shadow: 0px 2px #000;
+	}
+</style>
